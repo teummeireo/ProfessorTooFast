916821609ce406d7ceddb3841670c9b6b5e633e9
Hong Yoongi <hyg4work@gmail.com>
2024-12-26 10:33:53 +0900

    [add] #1 백엔드 초기 구조 설정완료.

---
+++ src/main/java/com/ptf/dao/StatisticsDAO.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.ptf.dao;
+
+/**
+ * Description : 클래스에 대한 설명을 입력해주세요.<br>
+ * Date : 2024. 12. 26.<br>
+ * History :<br>
+ * - 작성자 : Kosta, 날짜 : 2024. 12. 26., 설명 : 최초작성<br>
+ *
+ * @author Kosta
+ * @version 1.0
+ */
+public class StatisticsDAO {
+
+}
